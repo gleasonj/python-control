6a3e0e965dc2e1eb994c205721a688971c4e554d
gleasonj <>
2021-02-01 15:49:14 -0700

    updated point containment for special sets

--- control/sets/special.py
+++ control/sets/special.py
@@ -4,7 +4,13 @@ class Empty():
     def contains(self, x: np.ndarray):
         ''' 
         '''
-        check_valid_contains_points(x)
+        if not isinstance(x, np.ndarray):
+            raise TypeError('Point(s) must be a 1d numpy array or 2d numpy '
+                'array where each column represents a point.')
+
+        if len(x.shape) > 2:
+            raise TypeError('Point(s) must be a 1d numpy array or 2d numpy '
+                'array where each column represents a point.')
 
         if len(x.shape) == 2:
             return np.zeros(x.shape[1], dtype=bool)
@@ -13,7 +19,13 @@ class Empty():
 
 class Universe():
     def contains(self, x: np.ndarray):
-        check_valid_contains_points(x)
+        if not isinstance(x, np.ndarray):
+            raise TypeError('Point(s) must be a 1d numpy array or 2d numpy '
+                'array where each column represents a point.')
+
+        if len(x.shape) > 2:
+            raise TypeError('Point(s) must be a 1d numpy array or 2d numpy '
+                'array where each column represents a point.')
         
         if len(x.shape) == 2:
             return np.ones(x.shape[1], dtype=bool)
